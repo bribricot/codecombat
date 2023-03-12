727c3efcc1230218b9ca20a889fcf0c025a204aa
bribricot <kraemer.lucie@gmail.com>
2023-03-12 21:18:15 +0100

    Trim, split, length, méthode push dans un array vide

--- cloudripmountain/blue/62b_anonymous-bank.js
+++ cloudripmountain/blue/62b_anonymous-bank.js
@@ -1,15 +1,11 @@
 /*
-To get to the treasures you need 4 passwords. Luckily, we caught a scout that had the passwords.
-Now, we just need to decipher the message to get them.
-Words in the message are split by ";" and can be surrounded by redundant whitespaces.
-Also, we know that the passwords in the message only have 5 letters.
+Pour accéder aux trésors, vous avez besoin de 4 mots de passe. Heureusement, nous avons attrapé un éclaireur qui avait les mots de passe. Nous avons  besoin de déchiffrer le message pour les obtenir. Les mots du message sont séparés par ";" et peuvent être entourés de whitespaces redondants. Les mots de passe dans le message n'ont que 5 lettres.
 
-In this level you need to use various skills from recent levels.
-You will want to make use the following methods:
+Methodes importantes : 
 
-someStr.split(separator); // To get an array of words;
-word.trim(); // To remove leading and trailing whitespaces;
-cleanedWord.length; // To get the word's length
+someStr.split(separator); // Pour obtenir un tableau de mot
+word.trim(); // Enlever les whitespaces redondants
+cleanedWord.length; // Obtenir sa length
 
 */
 
@@ -21,26 +17,25 @@ let encodedMessage = hero.findNearest(hero.findFriends()).message;
 let passwordArray = [];
 // All real passwords have 5 letters.
 let passwordLength = 5;
-
 // Split encodedMessage by using a ";" and save the words in a variable.
-
+let words = encodedMessage.split(";");
 // Iterate over all of the words.
-
+for (let i = 0; i < words.length; i++) {
     // Trim each word to remove whitespace.
-    
+    let wordsTrimmed = words[i].trim();
     // If the length of the cleaned word is 5:
-    
+    if (wordsTrimmed.length === 5)
         // Push the cleaned word to passwordArray:
-        
-
+        passwordArray.push(wordsTrimmed);
+}
 
 // Move to each mark and say the correct password:
-var marksPos = [{x: 12, y: 14}, {x: 38, y: 38},
+let marksPos = [{x: 12, y: 14}, {x: 38, y: 38},
     {x: 42, y: 16}, {x: 54, y: 12}];
-for (let i = 0; i < 4; i++) {
-    let pos = marksPos[i];
+for (let j = 0; j < 4; j++) {
+    let pos = marksPos[j];
     hero.moveXY(pos.x, pos.y);
-    let password = passwordArray[i];
+    let password = passwordArray[j];
     if (password) {
         hero.say(password);
     } 
