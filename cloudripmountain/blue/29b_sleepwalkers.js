9e9804d9cd7452bbb72cb794458466cda51eb3a1
bribricot <kraemer.lucie@gmail.com>
2023-06-30 19:14:59 +0200

    Repérage NESW, condidtionnal advance in a map where every * steps I do smph in motion while the conditions predict where I go beetween x and y axis

--- cloudripmountain/blue/29b_sleepwalkers.js
+++ cloudripmountain/blue/29b_sleepwalkers.js
@@ -1,1 +1,21 @@
-//Locked
+// Our sleepwalking peasants are returning.
+// But sleeping yetis are also coming.
+// DON'T WAKE THEM UP!
+// Build fences to let peasants through and stop yetis.
+
+
+// Senick's prepared the grid map how to build fences.
+var hunter = hero.findNearest(hero.findFriends());
+var fenceMap = hunter.getMap();
+
+// This function converts grid into XY coordinates.
+function convertCoor(row, col) {
+    return {x: 34 + col * 4, y: 26 + row * 4};
+}
+
+
+// Iterate over fenceMap and build at fence at all 1s.
+
+
+// Move back to the village after building the fences.
+
