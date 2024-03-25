e28d39bbb5d12bf22861aa3db95834b95b9b23ec
bribricot <kraemer.lucie@gmail.com>
2024-03-25 12:08:01 +0100

    Double boucle avec une update en dehoors du if

--- cloudripmountain/blue/31b_snowdrops.js
+++ cloudripmountain/blue/31b_snowdrops.js
@@ -1,9 +1,57 @@
-// We need to clear the forest of traps!
-// The scout prepared a map of the forest.
-// But be careful where you shoot! Don't start a fire.
+/*
+We need to clear the forest of traps!
+The scout prepared a map of the forest.
+But be careful where you shoot! Don't start a fire.
 
+Spring is coming! The time for the big cleaning!
+We need to clear the forest and we have the cannon and the forest map for that.
+But the map has rows with different lengths, so be sure before a shot.
+We can't afford to shoot everywhere - ammo is expensive and we have just enough to clean traps.
+
+The map is an array of arrays, where 1 is clear and 0 is a trap. Like:
+[[1, 1, 0],
+ [0, 1],
+ [1, 1, 0, 0, 1]]
+
+Say the row and the column of the map where the artillery should shoot.
+We prepared the cell coordinates (you'll find them in the sample code) which you need to check.
+The map is an array of arrays, where 1 is clear and 0 is a trap. Like:
+let forestMap = [
+    [1, 1, 0],
+    [0, 1],
+    [1, 1, 0, 0, 1]]
+
+As we can see rows of that arrays have different lengths. 
+So we need to check the correctness of coordinates before to get access, else you can get an error.
+
+// The correct row, but the wrong column:
+forestMap[0][4] // undefined
+// The wrong row, any column
+forestMap[5][1] // TypeError, because forestMap[5] is undefined
+
+It's a good practice to check indexes before access them if you aren't sure it.
+For example, if you get access to random cell by random or outer source coordinates.
+
+To check the existence of the cell you can check if it's less than the length of the array
+(We don't consider negative indexes here):
+
+let row = 1;
+let col = 3;
+if (row < forestMap.length) {
+    // if 'row' is correct index
+    if (col < forestMap[row].length) {
+        // now we can get the cell
+        hero.say(forestMap[row][col]);
+    }
+}
+It can be written shorter with logic operators:
+// The second part is checked only if the first is true
+if (row < forestMap.length && col < forestMap[row].length) {
+        hero.say(forestMap[row][col]);
+}
+*/
 // Get the map of the forest.
-var forestMap = hero.findNearest(hero.findFriends()).forestMap;
+let forestMap = hero.findNearest(hero.findFriends()).forestMap;
 
 // The map is a 2D array where 0 is a trap.
 // The first sure shot.
@@ -11,15 +59,15 @@ hero.say("Row " + 0 + " Column " + 1 + " Fire!");
 
 // But for the next points, check before shooting.
 // There are an array of points to check.
-var cells = [{row: 0, col: 4}, {row: 1, col: 0}, {row: 1, col: 2}, {row: 1, col: 4},
+let cells = [{row: 0, col: 4}, {row: 1, col: 0}, {row: 1, col: 2}, {row: 1, col: 4},
     {row: 2, col: 1}, {row: 2, col: 3}, {row: 2, col: 5}, {row: 3, col: 0},
     {row: 3, col: 2}, {row: 3, col: 4}, {row: 4, col: 1}, {row: 4, col: 2},
     {row: 4, col: 3}, {row: 5, col: 0}, {row: 5, col: 3}, {row: 5, col: 5},
     {row: 6, col: 1}, {row: 6, col: 3}, {row: 6, col: 4}, {row: 7, col: 0}];
 
-for (var i = 0; i < cells.length; i++) {
-    var row = cells[i].row;
-    var col = cells[i].col;
+for (let i = 0; i < cells.length; i++) {
+    let row = cells[i].row;
+    let col = cells[i].col;
     // If row is less than forestMap length:
     
         // If col is less than forestMap[row] length:
--- cloudripmountain/blue/51b_distracting-dungeon.js
+++ cloudripmountain/blue/51b_distracting-dungeon.js
@@ -1,30 +1,84 @@
-// Navigate the mountain maze with a peasant pal.
-// Advanced while-loop usage is required!
-
+/* Navigate the mountain maze with a peasant pal.
+Advanced while-loop usage is required!
+This level focuses on advanced while-loops usage. Listed below are concepts to help visualize what kind of loops are involved.
 function moveBothTo(point) {
     while(hero.distanceTo(point) > 1) {
         hero.move(point);
         hero.command(peasant, "move", point);
     }
 }
+- Moving multiple using towards a point.
+- Defeating all at any given stage before continuing through the rest of a loop.
+- Attacking an enemy until it is defeated.
+- Waiting until a peasant catches up with a potentially faster hero.
 
-var peasant = hero.findNearest(hero.findFriends());
+1) An example of moving multiple units towards a point:
+
+let point = {x: 20, y: 20};
+let friends = hero.findFriends(); // In this level, there is only 1 friend!
+while(hero.distanceTo(point) > 0.5) {
+    hero.move(point); 
+    // The hero.move() method allows the hero to perform actions inbetween steps!
+    for(let i = 0; i < friends.length; i++) { 
+    // In this level, you won't need to use a for-loop with only 1 friend.
+        hero.command(friends[i], "move", point);
+    }
+}
+
+2) An example of defeating all enemies inside a while loop:
+
+while(true) {
+    hero.moveXY(hero.pos.x + 20, hero.pos.y);
+    let enemy = hero.findNearestEnemy();
+    while(enemy) {
+        hero.attack(enemy);
+        enemy = hero.findNearestEnemy(); // Update the variable for the inner while-loop to verify.
+    }
+}
+
+3) An example of defeating an enemy before moving to the next one:
+let enemy = hero.findNearestEnemy();
+while(enemy.health > 0) {
+    hero.attack(enemy); // This will continue to attack the enemy until it runs out of health.
+}
+
+4) An example of checking the distance with a unit other than the hero;
+
+let friend = hero.findNearest(hero.findFriends());
+let point = {x:20, y:20};
+while(friend.distanceTo(point) > 0.5) { // Note nothing much changes other than checking the distance from the peasant to the point.
+    hero.move(point);
+    hero.command(friend, "move", point);
+}
+
+*/function moveBothTo(point) {
+    while(hero.distanceTo(point) > 1) {
+        hero.move(point);
+        hero.command(peasant, "move", point);
+    }
+}
+
+let peasant = hero.findNearest(hero.findFriends());
 while(true) {
     // Command your friend to build a decoy towards x + 1:
     hero.command(peasant, "buildXY", "decoy", peasant.pos.x + 2, peasant.pos.y);
-    var nextPoint = {x: hero.pos.x, y: hero.pos.y + 28};
+    let nextPoint = {x: hero.pos.x, y: hero.pos.y + 28};
     moveBothTo(nextPoint);
     // Create a new x/y object +28 units away in the x dir:
     nextPoint = {x: hero.pos.x + 28, y: hero.pos.y};
     // Find the nearest enemy:
-    
+    let enemy = hero.findNearestEnemy();
     // While there is an enemy:
-    
+    while(enemy) {
         // While the enemy's health is > 0:
-        
+        while(enemy.health > 0) {
             // Attack the enemy:
-            
+            if (enemy) {
+                hero.attack(enemy);
+            }
+        }
         // Update the variable to the next nearest enemy:
-        
+        enemy = hero.findNearestEnemy();
+    }
     moveBothTo(nextPoint);
 }
